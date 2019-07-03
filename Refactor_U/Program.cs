9faa427fa64418d94dcacdce3bf209f3b98ee5dc
Stefan Stasiewicz <Stefan.Stasiewicz@toiohomai.ac.nz>
2019-07-03 17:38:34 +1200

    Just Made

--- Refactor_U/Program.cs
+++ Refactor_U/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Refactor_U
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Console.WriteLine("Welcome to Toi Ohomai               ");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Please enter your first name :");
+            string fname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name  :");
+            string lname = Console.ReadLine();
+
+
+            Random rnd = new Random();
+
+            string studentid = rnd.Next(1000, 9999).ToString();
+
+            Console.Clear();
+
+            Console.WriteLine("Welome to Toi Ohomai               ");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Your name is       : {0} {1}", fname, lname);
+            Console.WriteLine("Your student id is : {0}", studentid);
+
+            Console.ReadLine();
         }
+
     }
 }
